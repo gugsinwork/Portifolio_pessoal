3f03acaf1655c56b8a0b8d4f0f991a9af119d8f2
gugs(Gustavo.L) <123033640+gugsinwork@users.noreply.github.com>
2023-10-18 15:52:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-function calcularImovel(metragem, quartos){
-
+function calcularImovel() {
     let m2 = 3000;
-        let preco = 0;
-        quartos = document.getElementById('quarto').value
-        metragem = document.getElementById('m2').value
-        switch (metragem, quartos) {
-            case 1:
-                default:
-                    window.alert(`o preço é R$${preco}`)
-                    break;
-            case 2:
-    
-                preco = metragem * (m2*1.2)
-                window.alert(`o preço é R$${preco}`)
-    
-            case 3:
-    
-                preco = metragem * (m2*1.5)
-                window.alert(`o preço é R$${preco}`)
-                break;
-            case quartos > 3:
-    
-                preco = metragem * (m2*2)
-                window.alert(`o preço é R$${preco}`)
-                break;
-        }
-        return preco
+    let preco = 0;
+    let quartos = document.getElementById('quarto').value;
+    let metragem = document.getElementById('m2').value;
+
+    if (metragem <= 0 || quartos <= 0) {
+        window.alert('Valores inválidos. Certifique-se de preencher metragem e quartos com valores positivos.');
+    } else if (metragem < 100) {
+        preco = metragem * (m2 * 1.2);
+        window.alert(`O preço é R$${preco}`);
+    } else if (metragem < 200) {
+        preco = metragem * (m2 * 1.5);
+        window.alert(`O preço é R$${preco}`);
+    } else {
+        preco = metragem * (m2 * 2);
+        window.alert(`O preço é R$${preco}`);
     }
+    return preco;
+}
